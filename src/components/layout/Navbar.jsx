ff97c61d0ccc70f33dbe6157c7f299b93230c26c
COZYkrish <cozykrish2916@gmail.com>
2026-03-03 20:15:53 +0530

    added navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <div className="h-16 bg-white/5 backdrop-blur-lg border-b border-white/10 flex items-center justify-between px-8 ml-64">
+      <h2 className="text-lg text-gray-300">Welcome Back 👋</h2>
+      <div className="text-cyan-400 font-semibold">Krish</div>
+    </div>
+  )
+}
+
+export default Navbar
