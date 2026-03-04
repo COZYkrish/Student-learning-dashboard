e806d5836eba4afda1a67d4d492b82adfafbfac9
COZYkrish <cozykrish2916@gmail.com>
2026-03-04 14:31:55 +0530

    Day2 Commit1: Setup Course Context API for global course state

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { CourseProvider } from "./context/CourseContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <CourseProvider>
+      <App />
+    </CourseProvider>
+  </React.StrictMode>
+);
