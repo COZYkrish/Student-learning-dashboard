c79c4331a4f18698f6fb18b4a6aaec970758e60d
COZYkrish <cozykrish2916@gmail.com>
2026-03-04 14:53:02 +0530

    Day2 Commit7: Add localStorage persistence for course data

--- src/context/CourseContext.jsx
+++ src/context/CourseContext.jsx
@@ -1,11 +1,17 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { sampleCourses } from "../data/sampleCourses";
-
 export const CourseContext = createContext();
 
 export const CourseProvider = ({ children }) => {
 
-  const [courses, setCourses] = useState(sampleCourses);
+  const [courses, setCourses] = useState(() => {
+  const saved = localStorage.getItem("courses");
+  return saved ? JSON.parse(saved) : sampleCourses;
+});
+
+  useEffect(() => {
+    localStorage.setItem("courses", JSON.stringify(courses));
+  }, [courses]);
 
   const addCourse = (course) => {
     setCourses([...courses, course]);
