5b83b12c33ff20753b7c267f6ea2ca5a05d02d61
COZYkrish <cozykrish2916@gmail.com>
2026-03-06 18:47:33 +0530

    feat: add content to analytics pages

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,5 +1,23 @@
+import { motion } from "framer-motion"
+import AnalyticsChart from "../components/dashboard/AnalyticsChart"
+
 function Analytics() {
-  return <div className="text-3xl font-bold">Analytics 📊</div>
+
+  return (
+    <motion.div
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      className="p-8"
+    >
+
+      <h1 className="text-4xl font-bold mb-10">
+        Analytics 📊
+      </h1>
+
+      <AnalyticsChart />
+
+    </motion.div>
+  )
 }
 
 export default Analytics
