bcce179fca321660e558cbc8f1736ecb81f2f17e
COZYkrish <cozykrish2916@gmail.com>
2026-03-06 18:46:52 +0530

    feat: add content to courses pages

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,5 +1,44 @@
+import { useContext } from "react"
+import { motion } from "framer-motion"
+import CourseCard from "../components/ui/CourseCard"
+import { CourseContext } from "../context/CourseContext"
+
 function Courses() {
-  return <div className="text-3xl font-bold">Courses 📚</div>
+
+  const { courses } = useContext(CourseContext)
+
+  return (
+    <motion.div
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      className="p-8"
+    >
+
+      <h1 className="text-4xl font-bold mb-10">
+        Courses 📚
+      </h1>
+
+      {courses.length === 0 ? (
+        <p className="text-gray-400">
+          No courses added yet.
+        </p>
+      ) : (
+
+        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+
+          {courses.map(course => (
+            <CourseCard
+              key={course.id}
+              course={course}
+            />
+          ))}
+
+        </div>
+
+      )}
+
+    </motion.div>
+  )
 }
 
 export default Courses
