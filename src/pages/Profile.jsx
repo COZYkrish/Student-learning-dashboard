e65aa391c5265887ea229cd87053c66c5bb67ba9
COZYkrish <cozykrish2916@gmail.com>
2026-03-06 18:47:56 +0530

    feat: add content to courses analytics and profile pages

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,34 @@
+import { motion } from "framer-motion"
+
 function Profile() {
-  return <div className="text-3xl font-bold">Profile 👤</div>
+
+  return (
+    <motion.div
+      initial={{opacity:0}}
+      animate={{opacity:1}}
+      className="p-8"
+    >
+
+      <h1 className="text-4xl font-bold mb-10">
+        Profile 👤
+      </h1>
+
+      <div className="bg-slate-900 p-6 rounded-xl w-80">
+
+        <p className="text-gray-400">Name</p>
+        <h2 className="text-xl font-semibold mb-4">
+          Krish
+        </h2>
+
+        <p className="text-gray-400">Role</p>
+        <h2 className="text-xl font-semibold">
+          Student
+        </h2>
+
+      </div>
+
+    </motion.div>
+  )
 }
 
 export default Profile
